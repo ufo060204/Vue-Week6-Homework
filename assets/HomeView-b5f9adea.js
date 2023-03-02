b216827e8c709daf6b3d205b4cfcb5489158af5e
ufo060204 <ufo060204@gmail.com>
2023-03-03 00:42:28 +0800

    deploy

---
+++ assets/HomeView-b5f9adea.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-09c6781a.js";const t={components:{}},c={class:"home"};function n(r,a,_,p,i,m){return o(),s("div",c," 這是首頁 ")}const f=e(t,[["render",n]]);export{f as default};
